3f0db3f275b636d0e085fced3d16c912dee90d9a
= <=>
2020-03-04 22:27:41 +0100

    refactoring

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import flask
 from flask import request, jsonify
-from dbBroker import dbBroker
+from tasksDAO import tasksDAO
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -18,15 +18,10 @@ def page_not_found(e):
 # A route to return all of the available entries in our catalog.
 @app.route('/tasks/', methods=['GET'])
 def api_all():
-    dbB = dbBroker()
-    all_books = dbB.query('SELECT * FROM tasks;')
-    return jsonify(all_books)
+    return jsonify(tasksDAO().getAll())
 
 @app.route('/tasks/<int:task_id>', methods=['GET'])
 def api_book(task_id):
-    query = f"SELECT * FROM tasks WHERE id={task_id};"
-    dbB = dbBroker()
-    task = dbB.query(query)
-    return jsonify(task)
+    return jsonify(tasksDAO().get(task_id))
 
 app.run()
