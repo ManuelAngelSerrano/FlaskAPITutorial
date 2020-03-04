a96b671317d9a51e3575472dccd27e6d89f80594
ManuelAngelSerrano <Manuel.Serrano@uclm.es>
2020-03-04 11:55:03 +0100

    api all

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -26,11 +27,11 @@ books = [
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
-
+<p>A prototype API for distant reading of science fiction novels.</p>
+<p>/books/all para ver todos los libros</p>''' 
 
 # A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route('/books/all', methods=['GET'])
 def api_all():
     return jsonify(books)
 app.run()
