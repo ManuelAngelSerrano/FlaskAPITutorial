f08ec68c6ec70b4ff58d16831af48d79cfb30d34
= <=>
2020-03-04 21:55:35 +0100

    dbBroker

--- api.py
+++ api.py
@@ -1,31 +1,9 @@
 import flask
 from flask import request, jsonify
-import sqlite3
-
-db = './tasks.db'
+from dbBroker import dbBroker
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-class dbBroker:
-    def __init__(self, database):
-        self.db = database
-        self.conn = sqlite3.connect(self.db)
-        self.conn.row_factory = self.dict_factory
-        self.cursor = self.conn.cursor()
-
-    def dict_factory(self, cursor, row):
-        d = {}
-        for idx, col in enumerate(cursor.description):
-            d[col[0]] = row[idx]
-        return d
-    
-    def query(self, consulta):
-        return self.cursor.execute(consulta).fetchall()
-
-    def execute(self, consulta):
-        self.cursor.execute(consulta)
-        self.cursor.commit()
 
 @app.route('/', methods=['GET'])
 def home():
@@ -40,49 +18,15 @@ def page_not_found(e):
 # A route to return all of the available entries in our catalog.
 @app.route('/tasks/', methods=['GET'])
 def api_all():
-    dbB = dbBroker(db)
+    dbB = dbBroker()
     all_books = dbB.query('SELECT * FROM tasks;')
     return jsonify(all_books)
 
 @app.route('/tasks/<int:task_id>', methods=['GET'])
 def api_book(task_id):
     query = f"SELECT * FROM tasks WHERE id={task_id};"
-    dbB = dbBroker(db)
+    dbB = dbBroker()
     task = dbB.query(query)
     return jsonify(task)
-
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_filter():
-    query_parameters = request.args
-
-    id = query_parameters.get('id')
-    published = query_parameters.get('published')
-    author = query_parameters.get('author')
-
-    query = "SELECT * FROM books WHERE"
-    to_filter = []
-
-    if id:
-        query += ' id=? AND'
-        to_filter.append(id)
-    if published:
-        query += ' published=? AND'
-        to_filter.append(published)
-    if author:
-        query += ' author=? AND'
-        to_filter.append(author)
-    if not (id or published or author):
-        return page_not_found(404)
-
-    query = query[:-4] + ';'
-
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-
-    results = cur.execute(query, to_filter).fetchall()
-
-    return jsonify(results)
 
 app.run()
